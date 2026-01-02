1788df43e68ac73e6e7712f856886bc1768a213b
Keval Hirpara <kevalhirpara2003@gmail.com>
2026-01-02 14:29:19 +0530

    ad files

--- 1200/smae parity summands.cpp
+++ 1200/smae parity summands.cpp
@@ -8,11 +8,29 @@ int main(){
         int n, k;
         cin >> n >> k;
 
-        if(n < k || (n % 2 == 0 && k % 2 == 1)){
+        if(n < k){
             cout << "NO" << endl;
             continue;
         }
 
-        
+        int cnt = 0;
+        int parity = 0;
+        vector<int> nums(k);
+        for(int i=0;i<n;i++){
+            if(i < n-1){
+                nums[i] = 1;
+                cnt++;
+            }else{
+                nums[i] = n - cnt;
+                parity = 1;
+            }
+        }
+        if(parity == 1){
+            cout << "YES" << endl;
+            for(int i=0;i<n;i++){
+                cout << nums[i] << " ";
+            }
+            cout << endl;
+        }
     }
 }
